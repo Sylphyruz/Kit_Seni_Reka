ea01dced53500b6e7547b6769f3be3138e304052
Sylphyruz <147905851+Sylphyruz@users.noreply.github.com>
2023-10-14 14:51:09 +0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,133 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Gabungan dari satu titik ke satu titik yang lain di atas satu permukaan",
+    answer: "Garisan",
+    options: [
+      "Irama dan pergerakan",
+      "Garisan",
+      "Harmoni",
+      "Jalinan"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Jarak di antara dua objek",
+    answer: "Ruang",
+    options: [
+      "Kepelbagaian",
+      "Kontra",
+      "Ruang",
+      "Bentuk"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Kesan yang terdapat pada sesuatu permukaan",
+    answer: "Jalinan",
+    options: [
+      "Jalinan",
+      "Garisan",
+      "Penegasan",
+      "Kesatuan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Hasil pertemuan satu garisan dari pengkal ke hujungnya",
+    answer: "Rupa",
+    options: [
+      "Imbangan",
+      "Bentuk",
+      "Ruang",
+      "Rupa"
+    ]
+  },
+    {
+    numb: 5,
+    question: "satu pigmen yang bertindak balas apabila cahaya dipantul atau dipancar pada sesuatu permukaan",
+    answer: "Warna",
+    options: [
+      "Kontra",
+      "Warna",
+      "Penegasan",
+      "Kepelbagaian"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Unsur-unsur seni yang bertentangan atau bercanggah dalam sesuatu gubahan dan berfungsi untuk menarik perhatian, memberi fokus, mengelakkan kebosanan dan kesan pergerakan",
+    answer: "Kontra",
+    options: [
+      "Rupa",
+      "Warna",
+      "Penegasan",
+      "Kontra"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Hasil ulangan dan rentak unsur seni yang sesuai dan seimbang",
+    answer: "Harmoni",
+    options: [
+      "Harmoni",
+      "Kesatuan",
+      "Kepelbagaian",
+      "Jalinan"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Kesamaan dan kesepadanan dari segi berat, tarikan dan tumpuan",
+    answer: "Imbangan",
+    options: [
+      "Garisan",
+      "Jalinan",
+      "Irama dan pergerakan",
+      "Imbangan"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Penggabunggan prinsip rekaan dan unsur seni dalam usaha melahirkan sesuatu rekaan seni visual yang sempurna lagi baik. Ia selalunya menjurus kepada tema dan idea yang sama yang saling lengkap-melengkapi antara satu sama lain",
+    answer: "Kesatuan",
+    options: [
+      "Rupa",
+      "Warna",
+      "Ruang",
+      "Kesatuan"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Penggunaan pelbagai gaya, nilai, dan idea yang berbeza dalam sesuatu gubahan",
+    answer: "Kepelbagaian",
+    options: [
+      "Kepelbagaian",
+      "Kesatuan",
+      "Irama dan pergerakan",
+      "Penegasan"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
